b6cc04ea9fa97545e1330b3ae1ab32be7021fc67
Paul Pietkiewicz <pawel.pietkiewicz@gmail.com>
2012-05-15 12:36:14 +0100

    continue to generate playlist after getting blocked

--- src/wheremajamzat/grooveshark.py
+++ src/wheremajamzat/grooveshark.py
@@ -140,32 +140,35 @@ def download_songs(download_directory, song_list, aggressive=False):
 		artist = s[0]["ArtistName"]
 		track_path = os.path.join(download_directory, get_track_name(title, artist))
 		start_time = datetime.datetime.now()
-		if not os.path.exists(track_path):
-			wait_flag = True
-			wgets =  'wget --user-agent="%s" --referer=%s --post-data=streamKey=%s -O "%s" "http://%s/stream.php"' % (_useragent, _referer, stream["streamKey"], track_path, stream["ip"])
-			p = subprocess.Popen(wgets, shell=True)
-			p.wait()
-			try:
-				id3info = ID3(track_path)
-				id3info['TITLE'] = title
-				id3info['ARTIST'] = artist
-				id3info.write()
-				print 'ID3 tags written'
-			except InvalidTagError, message:
-				print "Invalid ID3 tag:", message
-		else:
-			print 'File %s already exists' % track_path
+		try:
+			if not os.path.exists(track_path):
+				wait_flag = True
+				wgets =  'wget --user-agent="%s" --referer=%s --post-data=streamKey=%s -O "%s" "http://%s/stream.php"' % (_useragent, _referer, stream["streamKey"], track_path, stream["ip"])
+				p = subprocess.Popen(wgets, shell=True)
+				p.wait()
+				try:
+					id3info = ID3(track_path)
+					id3info['TITLE'] = title
+					id3info['ARTIST'] = artist
+					id3info.write()
+					print 'ID3 tags written'
+				except InvalidTagError, message:
+					print "Invalid ID3 tag:", message
+			else:
+				print 'File %s already exists' % track_path
 			wait_flag = False
-		if not aggressive and song != song_list[-1]:
-			if wait_flag:
-				track_length = datetime.timedelta(seconds=get_song_length(track_path))
-				end_time = datetime.datetime.now()
-				to_wait = track_length - (end_time - start_time)
-				to_wait = to_wait.total_seconds() + 2
-			if not wait_flag:
-				to_wait = random.randrange(5, 12)
-			print "Sleeping for %d seconds" % to_wait
-			time.sleep(to_wait)
-		songs.append({'path': track_path, 'name': title, 'artist': artist})
+			if not aggressive and song != song_list[-1]:
+				if wait_flag:
+					track_length = datetime.timedelta(seconds=get_song_length(track_path))
+					end_time = datetime.datetime.now()
+					to_wait = track_length - (end_time - start_time)
+					to_wait = to_wait.total_seconds() + 2
+				if not wait_flag:
+					to_wait = random.randrange(5, 12)
+				print "Sleeping for %d seconds" % to_wait
+				time.sleep(to_wait)
+			songs.append({'path': track_path, 'name': title, 'artist': artist})
+		except TypeError: as e:
+			print "Just got banned by grooveshark, stopping download "
 
 	return songs
